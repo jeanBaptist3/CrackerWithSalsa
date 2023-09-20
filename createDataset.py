9cb38439aec33f1ae6c6b49787096647640d975f
Leonhard Wensch <lawensch42@gmail.com>
2023-09-20 20:01:18 +0200

    updated the createDataset.py
    added runtime and a generator instead of for loop

--- createDataset.py
+++ createDataset.py
@@ -1,21 +1,24 @@
 from datasets import Dataset
 import struct
 import gzip
+import time
 
 # Define the range of 32-bit integers
 min_int = -2**31  # Minimum 32-bit integer
 max_int = 2**31 - 1  # Maximum 32-bit integer
 
-# Generate a list of 32-bit integers
-integers = list(range(min_int, max_int + 1))
+start_time = time.time()
+# Define a generator function to produce binary data one item at a time
+def generate_binary_data():
+    for i in range(min_int, max_int + 1):
+        yield struct.pack('i', i)
 
-# Convert integers to bytes
-byte_data = [struct.pack('i', i) for i in integers]
-
-# Create a dataset
-dataset = Dataset.from_dict({"data": byte_data})
+print("--- %s seconds ---" % (time.time() - start_time))
+# Create a dataset using the generator
+dataset = Dataset.from_generator(generate_binary_data, output_signature={"data": "bytes"})
 
 # Specify a compression option when saving to disk
 dataset.save_to_disk("32_bit_integers_dataset_compressed", storage_options={"compress": "gzip"})
 
 print("Dataset with compression created and saved.")
+
