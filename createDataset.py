e45751de0d3842cf99d038a8788a6d5c2a071480
Leonhard Wensch <lawensch42@gmail.com>
2023-09-21 23:03:22 +0200

    file for loading and training of the tokenizer
    fixed tokenizer training and input

--- createDataset.py
+++ createDataset.py
@@ -9,29 +9,29 @@ from datasets import Dataset
 # batch size for memory regulation
 batch_size = 2**21
 
-min_int = -2**31 # Minimum 32-bit integer
-max_int = 2**31 -1 # Maximum 32-bit integer
+min_int = -2**22 # Minimum 32-bit integer
+max_int = 2**22# Maximum 32-bit integer
 output_directory = "data"
 
 # Function to generate a batch of data
 def generate_and_save_batch(start, end, filename):
     batch = []
     for i in range(start, end):
-        batch.append({"byte": struct.pack('i', i)})
+        batch.append(struct.pack('i', i))
 
         # Check if the batch is full
         if len(batch) == batch_size:
-            dataset = Dataset.from_dict({"data": batch})
+            dataset = Dataset.from_dict({"byte": batch})
             dataset.save_to_disk(f"{output_directory}/{filename}", storage_options={"compress": "gzip"})
             batch = []
 
     # Save any remaining data in the last batch
     if batch:
-        dataset = Dataset.from_dict({"data": batch})
+        dataset = Dataset.from_dict({"byte": batch})
         dataset.save_to_disk(f"{output_directory}/{filename}", storage_options={"compress": "gzip"})
 
 # Define the number of parallel workers (adjust as needed)
-num_workers = 32
+num_workers = 4
 
 # Create a ThreadPoolExecutor with the specified number of workers
 with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
@@ -39,17 +39,17 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
 
     # Divide the range into chunks for parallel processing
     chunk_size = (max_int - min_int) // num_workers
-
+    counter = 0
     for i in range(min_int, max_int, chunk_size):
         start = i
         end = min(i + chunk_size, max_int)
-        filename = f"allStrings_partial_{start}_{end}.arrow"
+        filename = f"allStrings_partial_{counter}.arrow"
         futures.append(executor.submit(generate_and_save_batch, start, end, filename))
+        counter = counter+1
 
 # Wait for all tasks to complete
 concurrent.futures.wait(futures)
 
-print("Dataset with compression created and saved in parallel batches.")
 
 
 #print("--- % seconds ----" % (time.time() - start_time))
---
+++ read_data_and_train_tokenizer.py
@@ -0,0 +1,55 @@
+import struct
+from datasets import Dataset
+from tokenizers import Tokenizer, trainers, pre_tokenizers, decoders
+from tokenizers.models import WordLevel
+# Load the dataset from the saved disk location
+fulldata = []
+
+
+checkpoint = "t5-small"
+#function to convert the bytes to binary strings
+def binary_to_bit_string(binary_data):
+    # Unpack as a 4-byte integer using 'i' format
+    byte_value = struct.unpack('i', binary_data)[0]
+    # Convert to binary and pad to 32 bits
+    if byte_value < 0:
+        # If the value is negative, convert to a 32-bit binary string with the sign bit
+        bit_string = bin(byte_value & 0xFFFFFFFF)[2:]
+    else:
+        # If the value is non-negative, convert to a 32-bit binary string without the sign bit
+        bit_string = bin(byte_value)[2:].zfill(32)
+    return bit_string
+
+
+for i in range(0,33) :
+    dataset = Dataset.load_from_disk(f"data/allStrings_partial_{i}.arrow")
+    for integer in dataset["byte"] :
+        fulldata.append(binary_to_bit_string(integer))
+
+print(len(fulldata))
+# Initialize a custom tokenizer
+tokenizer = Tokenizer(WordLevel())
+
+# Define a custom pre-tokenizer with rules to split on punctuation and recognize custom tokens
+tokenizer1 = pre_tokenizers.WhitespaceSplit()
+tokenizer2 = pre_tokenizers.CharDelimiterSplit(',')
+pre_tokenizer = pre_tokenizers.Sequence([tokenizer1,tokenizer2])
+
+tokenizer.pre_tokenizer = pre_tokenizer
+
+# Define a custom decoder
+decoder = decoders.ByteLevel()
+tokenizer.decoder = decoder
+
+
+training_data = fulldata
+
+# Train the tokenizer
+trainer = trainers.WordLevelTrainer(special_tokens=["[PAD]", "[CLS]", "[SEP]", "[MASK]", "[UNK]", "[NONC1]", "[NONC2]"])
+tokenizer.train_from_iterator(training_data, trainer=trainer)
+
+
+# Save the trained tokenizer to a file
+tokenizer.save("custom_tokenizer.json")
+
+print("Saved Tokenizer")
