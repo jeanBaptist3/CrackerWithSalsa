5840bbbaaaab0fe5961d0f1967a9ca05fefeb99d
Leonhard Wensch <lawensch42@gmail.com>
2023-09-21 14:52:52 +0200

    fixed createDataset
    now constant memory flow during creation

--- createDataset.py
+++ createDataset.py
@@ -1,35 +1,55 @@
 import struct
 import time
+import concurrent.futures
 from datasets import Dataset
 
-start_time = time.time();
-# Define the batch size
-batch_size = 2**8  # You can adjust this to your preferred batch size
+#start_time = time.time();
 
-min_int = 0 # Minimum 32-bit integer
-max_int = 2**24  # Maximum 32-bit integer
 
-# Generate data in batches
-data_batches = []
-batch = []
+# batch size for memory regulation
+batch_size = 2**21
 
-for i in range(min_int, max_int):
-    batch.append({"byte": struct.pack('i', i)})
+min_int = -2**31 # Minimum 32-bit integer
+max_int = 2**31 -1 # Maximum 32-bit integer
+output_directory = "data"
 
-    # Check if the batch is full
-    if len(batch) == batch_size:
-        data_batches.append(batch)
-        batch = []
+# Function to generate a batch of data
+def generate_and_save_batch(start, end, filename):
+    batch = []
+    for i in range(start, end):
+        batch.append({"byte": struct.pack('i', i)})
 
-# If there are remaining items in the last batch, add it
-if batch:
-    data_batches.append(batch)
+        # Check if the batch is full
+        if len(batch) == batch_size:
+            dataset = Dataset.from_dict({"data": batch})
+            dataset.save_to_disk(f"{output_directory}/{filename}", storage_options={"compress": "gzip"})
+            batch = []
 
-# Create a dataset from the list of data batches
-dataset = Dataset.from_dict({"data": data_batches})
+    # Save any remaining data in the last batch
+    if batch:
+        dataset = Dataset.from_dict({"data": batch})
+        dataset.save_to_disk(f"{output_directory}/{filename}", storage_options={"compress": "gzip"})
 
-# Save the dataset to disk with gzip compression
-dataset.save_to_disk("allStrings", storage_options={"compress": "gzip"})
+# Define the number of parallel workers (adjust as needed)
+num_workers = 32
 
-print("Dataset with compression created and saved.")
-print("--- % seconds ----" % (time.time() - start_time))
+# Create a ThreadPoolExecutor with the specified number of workers
+with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
+    futures = []
+
+    # Divide the range into chunks for parallel processing
+    chunk_size = (max_int - min_int) // num_workers
+
+    for i in range(min_int, max_int, chunk_size):
+        start = i
+        end = min(i + chunk_size, max_int)
+        filename = f"allStrings_partial_{start}_{end}.arrow"
+        futures.append(executor.submit(generate_and_save_batch, start, end, filename))
+
+# Wait for all tasks to complete
+concurrent.futures.wait(futures)
+
+print("Dataset with compression created and saved in parallel batches.")
+
+
+#print("--- % seconds ----" % (time.time() - start_time))
